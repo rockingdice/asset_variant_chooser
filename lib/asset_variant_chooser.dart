fcf3aa2a1eb6e24e919f46e4d6aa13c562514dc7
rockingdice <rockingdice@gmail.com>
2021-03-06 12:48:15 +0800

    pre-publish

--- lib/asset_variant_chooser.dart
+++ lib/asset_variant_chooser.dart
@@ -6,59 +6,13 @@ import 'package:flutter/widgets.dart';
 
 const double _kLowDprLimit = 2.0;
 
-class AssetVariantChooser {
-  static SplayTreeMap<double, String> candidates = SplayTreeMap();
-  static var _defaultRatioName = "";
-
-  /// Return the best choice of resolution variants for current device pixel
-  /// ratio in the String type.
-  /// Example: "1.5x"
-  static String chooseVariant(BuildContext context) {
-    var currentRatio = MediaQuery.of(context).devicePixelRatio;
-    String bestVariant =
-        _findBestVariant(candidates, currentRatio) ?? _defaultRatioName;
-    return bestVariant;
-  }
-
-  /// Return the best choice of resolution variants for current device pixel
-  /// ratio in the double type.
-  /// Example: 1.5
-  static double chooseNumericVariant(BuildContext context) {
-    var currentRatio = MediaQuery.of(context).devicePixelRatio;
-    String bestVariant =
-        _findBestVariant(candidates, currentRatio) ?? _defaultRatioName;
-    if (bestVariant == _defaultRatioName) {
-      bestVariant = assetName;
-    }
-
-    return _parseScale(bestVariant);
-  }
-
-  /// Set available variants for each supported asset resolution.
-  ///
-  /// [pixelRatios] should be provided as "3x", "1.5x"..etc. The order is
-  /// irrelevant.
-  /// [defaultRatioName] can be optionally used if you want to use a different
-  /// suffix for 1.0 ratio. [chooseVariant] will return this value if the ratio
-  /// is 1.0.,
-  static void setAvailableVariants(List<String> pixelRatios,
-      {String defaultRatioName = ""}) {
-    _defaultRatioName = defaultRatioName;
-    pixelRatios.forEach((element) {
-      var ratio = _parseScale(element);
-      candidates.addEntries([MapEntry(ratio, element)]);
-    });
-    candidates.clear();
-    if (!candidates.containsKey(1.0)) {
-      candidates.addEntries([MapEntry(1.0, _defaultRatioName)]);
-    }
-  }
-
+class _AssetVariantChooser {
   static const assetName = "";
 
   //copied from painting/image_resolution.dart
   static const double _naturalResolution = 1.0;
   static final RegExp _extractRatioRegExp = RegExp(r'/?(\d+(\.\d*)?)x$');
+
   static double _parseScale(String key) {
     if (key == assetName) {
       return _naturalResolution;
@@ -92,5 +46,66 @@ class AssetVariantChooser {
       return candidates[upper];
     else
       return candidates[lower];
+  }
+}
+
+class AssetVariantChooser {
+  static final AssetVariantChooser _singleton = AssetVariantChooser._internal();
+
+  factory AssetVariantChooser() {
+    return _singleton;
+  }
+
+  AssetVariantChooser._internal() {
+    _candidates.addAll({1.0: "", 2.0: "2x", 3.0: "3x"});
+  }
+
+  SplayTreeMap<double, String> _candidates = SplayTreeMap();
+  var _defaultRatioName = "";
+
+  /// Return the best choice of resolution variants for current device pixel
+  /// ratio in the String type.
+  /// Example: "1.5x"
+  String chooseVariant(BuildContext context) {
+    var currentRatio = MediaQuery.of(context).devicePixelRatio;
+    String bestVariant =
+        _AssetVariantChooser._findBestVariant(_candidates, currentRatio) ??
+            _defaultRatioName;
+    return bestVariant;
+  }
+
+  /// Return the best choice of resolution variants for current device pixel
+  /// ratio in the double type.
+  /// Example: 1.5
+  double chooseNumericVariant(BuildContext context) {
+    var currentRatio = MediaQuery.of(context).devicePixelRatio;
+    String bestVariant =
+        _AssetVariantChooser._findBestVariant(_candidates, currentRatio) ??
+            _defaultRatioName;
+    if (bestVariant == _defaultRatioName) {
+      bestVariant = _AssetVariantChooser.assetName;
+    }
+
+    return _AssetVariantChooser._parseScale(bestVariant);
+  }
+
+  /// Set available variants for each supported asset resolution.
+  ///
+  /// [pixelRatios] should be provided as "3x", "1.5x"..etc. The order is
+  /// irrelevant.
+  /// [defaultRatioName] can be optionally used if you want to use a different
+  /// suffix for 1.0 ratio. [chooseVariant] will return this value if the ratio
+  /// is 1.0.,
+  void setAvailableVariants(List<String> pixelRatios,
+      {String defaultRatioName = ""}) {
+    _defaultRatioName = defaultRatioName;
+    pixelRatios.forEach((element) {
+      var ratio = _AssetVariantChooser._parseScale(element);
+      _candidates.addEntries([MapEntry(ratio, element)]);
+    });
+    _candidates.clear();
+    if (!_candidates.containsKey(1.0)) {
+      _candidates.addEntries([MapEntry(1.0, _defaultRatioName)]);
+    }
   }
 }
